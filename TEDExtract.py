10fdef8b89085c3dc510678b936343156fb93a9a
Markus Näther <naether.markus@gmail.com>
2018-05-07 11:04:11 +0200

    Replaced usage of urllib by http.client + code cleanup.

--- TEDExtract.py
+++ TEDExtract.py
@@ -16,6 +16,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from six.moves import cPickle
 
+import http.client
+
 drop_out_lines = [
   "Subscribe to receive email notifications whenever new talks are published.",
   "Thanks! Please check your inbox for a confirmation email. ",
@@ -38,6 +40,9 @@ class TEDExtract(object):
     # Dictionary containing all talks
     self.all_talks = {}
 
+    self.header = { 'User-Agent' : 'TEDExtract script by naetherm' }
+    self.conn = http.client.HTTPSConnection('www.ted.com')
+
   def run(self):
     '''
     Receive all information from the ted page. 
@@ -50,7 +55,7 @@ class TEDExtract(object):
     else:
       # Collect all talks from all pages
       for p in range(1, self.args.max_pages + 1):
-        path = 'https://www.ted.com/talks?page={}'.format(p)
+        path = '/talks?page={}'.format(p)
 
         self._fetch_talk_list(path)
 
@@ -58,42 +63,55 @@ class TEDExtract(object):
         cPickle.dump(self.all_talks, fout)
 
     # DEBUG
-    
-    hdr= { 'User-Agent' : 'TEDExtract - some academic stuff' }
-    conn = http.client.HTTPConnection('www.ted.com')
     # Loop through all talks and download the content for all available languages
     for i in self.all_talks:
-      self._fetch_talk_content(conn, hdr, i)
-    conn.close()
+      self._fetch_talk_content(i)
+    self.conn.close()
 
   def _fetch_talk_list(self, path):
     '''
     This method is used to receive all 
     '''
     print("Reading talks of \'{}\'".format(path))
-    content = urllib.request.urlopen(path).read()
+    content = self._get_content2(path)
     soup = BeautifulSoup(content)
     talks = soup.find_all("a", class_='ga-link')
     for i in talks:
       if i.attrs['href'].find('/talks/') == 0 and self.all_talks.get(i.attrs['href']) != 1:
         self.all_talks[i.attrs['href']] = 1
 
-    sleep(self.args.delay)
+    sleep(2)
+
+  def _get_content2(self, uri):
+    '''
+    Reading and returning the content of the provided uri.
+    '''
+    self.conn.request('GET', uri, headers=self.header)
+    resp = None
+    # do try to fetch the content of the uri
+    while True:
+      try:
+        resp = self.conn.getresponse()
+      except ConnectionError as e:
+        print("Received an error from server, wait for {} seconds.".format(self.args.delay))
+        sleep(self.args.delay)
+      else:
+        break
+
+    return resp.read()
     
-  def _fetch_talk_content(self, conn, hdr, talk):
+  def _fetch_talk_content(self, talk):
     '''
     '''
     # Extract the talk name
     talkname = talk[7:]
 
     if os.path.isfile(os.path.join(self.args.output, talkname + '.csv')):
-      print("Already there, skip {}".format(talk))
+      print("Already downloaded, skip {}".format(talk))
     else:
       # The data frame object for saving all languages
-      req = urllib.request.urlopen('https://ted.com' + talk + '/transcript').read()
-      #conn.request('GET', talk + '/transcript', headers=hdr)
-      #req = conn.getresponse().read()
-      print("Search URL: {}".format(talk + '/transcript'))
+      req = self._get_content2(talk + '/transcript')
+      print("Reading transcriptions of {}".format(talk + '/transcript'))
       soup = BeautifulSoup(req)
 
       df = pd.DataFrame()
@@ -103,9 +121,7 @@ class TEDExtract(object):
           lang = i.attrs['hreflang']
           path = i.attrs['href']
           print("Path: {}".format(path))
-          #conn.request('GET', path, headers=hdr)
-          #r1 = conn.getresponse().read()
-          r1 = urllib.request.urlopen(path).read()
+          r1 = self._get_content2(path)
           soup1 = BeautifulSoup(r1)
           text_talk = []
           for i in soup1.findAll('p',class_=''):
@@ -128,6 +144,6 @@ class TEDExtract(object):
           # This break is necessary, otherwise we'll receive a 429. THere would be
           # a potential wordaround as noted by the commented out code snippets, but
           # unfortunately this is not working
-          sleep(self.args.delay)
+          sleep(2)
 
       df.to_csv(os.path.join(self.args.output, talkname + '.csv'), sep='\t', encoding='utf-8')
